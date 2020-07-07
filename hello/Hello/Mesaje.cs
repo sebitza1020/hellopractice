d2b6e036eb836b27989b688911cd2f156cede438
sebitza1020 <61789114+sebitza1020@users.noreply.github.com>
2020-07-07 12:47:51 +0300

    Initial commit (Push).

---
+++ hello/Hello/Mesaje.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hello
+{
+    internal class Mesaje
+    {
+        internal static void Afiseaza(string mesaj)
+        {
+            Console.WriteLine(mesaj);
+        }
+
+        internal static void Afiseaza(string mesaj, string limba)
+        {
+            switch (limba)
+            {
+                case "EN": Console.WriteLine("Hello world!"); break;
+                case "RO": Console.WriteLine("Salut lume!"); break;
+                case "HU": Console.WriteLine("Helló Világ"); break;
+                default:
+                    Console.WriteLine("Salut default!");
+                    break;
+            }
+        }
+    }
+}
---
+++ hello/Hello/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mesaje.Afiseaza("Salut lume!", "EN");
+            Mesaje.Afiseaza("Salut lume!", "RO");
+            Mesaje.Afiseaza("Salut lume!", "HU");
+            Mesaje.Afiseaza("Sanatate!");
+        }
+    }
+}
